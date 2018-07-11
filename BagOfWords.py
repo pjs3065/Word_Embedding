865ca2e205d338639d667dcdafe8421422283378
Jaeseong Park(JSP) <tmakdlfwotjd@naver.com>
2018-07-12 01:34:17 +0900

    make function 1. html 없애기 2. 문자 아닌거 없애기 3. 워드 단위로 쪼개기 4. 불용어 없애기 5. 워드 중에 불용어 없애기 6. 없앤 워드들 한단락으로 만들기

--- BagOfWords.py
+++ BagOfWords.py
@@ -6,22 +6,32 @@ import nltk
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 
+def review_to_words(raw_review):
+    # 1. remove html
+    review_text = BeautifulSoup(raw_review).get_text()
 
+    # 2. remove non-letters
+    letters_only = re.sub("[^a-zA-Z]"," ",  review_text)
+
+    # 3. convert to lower case, split into individual words
+    words = letters_only.lower().split()
+
+    # 4. In python, searching a set is much faster than searching a list, so convert the stop words to a set
+    stops = set(stopwords.words("english"))
+
+    #5. remove stop words
+    meaningful_words = [w for w in words if not w in stops]
+
+    #6. Join the words back into on string separated by space. and return the result.
+    return(" ".join( meaningful_words))
+
+# main
 train = pd.read_csv("labeledTrainData.tsv", header=0, \
                     delimiter="\t", quoting=3)
 
-example1 = BeautifulSoup(train["review"][0])
+clean_review = review_to_words(train["review"][0])
+print(clean_review)
 
-letters_only = re.sub("[^a-zA-Z]",
-                      " ",
-                      example1.get_text() )
 
-lower_case = letters_only.lower()
-words = lower_case.split()
 
-#nltk.download()  # Download text data sets, including stop words
 
-print (train["review"][0])
-print(example1.get_text())
-print(letters_only)
-print(stopwords.words("korea"))
